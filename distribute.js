157d6181e35d6c74310c91f80c4a6d325b8ed75e
ravina-mestry <x21120501@student.ncirl.ie>
2022-04-10 13:00:02 +0100

    Distribution working

--- distribute.js
+++ distribute.js
@@ -1,11 +1,22 @@
-// read in a file of accounts DONE
-// count the number of accounts DONE
-// get the remaining supply belonging to token owner DONE
+// read in a file of accounts
+// count the number of accounts
+// get the remaining supply belonging to token owner
+// get 5% of remaining supply
+// loop through accounts running a token transfer of 5%
 // divide 5% of remaining supply between the accounts
 // collect tea and medals
 
 let fs = require("fs");
 let contract = require("./contract.js");
+let BigNumber = require("big-number");
+
+require('dotenv').config();
+
+
+//infuraToken = process.env.INFURA_TOKEN;
+contractAddress = process.env.CONTRACT_ADDRESS;
+ownerAddress = process.env.OWNER_ADDRESS;
+// privateKey = Buffer.from(process.env.SUPER_SECRET_PRIVATE_KEY, 'hex');
 
 const doDistro = async() => {
 
@@ -15,6 +26,21 @@ const doDistro = async() => {
 
     let symbol = await contract.getSymbol();
     console.log(`token symbol is ${symbol}`);
+
+    let remainingSupply = await contract.getBalance(ownerAddress);
+    let ownerBalance = new BigNumber(remainingSupply);
+
+    let fivepercentOfBalance = ownerBalance.div(20);
+    console.log(`five % of remaining supply is ${fivepercentOfBalance}`);
+
+    let numberOfAddresses = distributionAddresses.length;
+    let distributionAmount = fivepercentOfBalance.div(numberOfAddresses);
+    
+    for (looper = 0; looper < numberOfAddresses; looper++) {
+        console.log(`about to distribute ${distributionAmount} ${symbol} to ${distributionAddresses[looper]}`)
+        
+        let returnValue = await contract.transferToken(ownerAddress, distributionAddresses[looper], distributionAmount);
+    }
 }
 
 doDistro();
